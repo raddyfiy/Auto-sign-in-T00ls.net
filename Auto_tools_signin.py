af472762e920030efb5612c7c692f75843622197
raddyfiy <1606020111@hhu.edu.cn>
2020-07-07 17:51:07 +0800

    add cookie login
    add cookie login

--- Auto_tools_signin.py
+++ Auto_tools_signin.py
@@ -5,7 +5,13 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Linux; Android 7.1.1; E6883 Build/32.4.A.1.54; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/80.0.3987.99 Mobile Safari/537.36',   
     'Referer':'https://www.t00ls.net/members-profile-12894.html'
 }
-
+logindata={
+"action" :"login",
+"username":"yourname",  #填你的用户名，不要填ID
+"password":"yourMD5",   #密码的MD5值
+"questionid":1, #问题编号，对照下面注释填写，若没有设置提问则此处随便填写
+"answer":""  #输入回答，若没有设置提问则此处随便填写，或不填
+}
 # questionid
 # 1 母亲的名字
 # 2 爷爷的名字
@@ -17,29 +23,38 @@ headers = {
 
 def login(session):
     loginurl="https://www.t00ls.net/login.json"
-    logindata={
-    "action" :"login",
-    "username":"yourname",  #填你的用户名，不要填ID
-    "password":"yourMD5",   #你密码的MD5值
-    "questionid":1, #问题编号，对照上面注释填写，若没有设置提问则此处随便填写
-    "answer":""  #输入回答，若没有设置提问则此处随便填写
-    }
     response=session.post(url=loginurl,data=logindata,headers=headers)
     responsejson=json.loads(response.text)
+    with open("cookiefile","wb") as fn:
+        pickle.dump(session.cookies,fn)
     try:
-        return responsejson["status"]
+        return responsejson["status"],responsejson["formhash"]
     except:
         return "login_error"
 
-def signin(session):
+def cookielogin(session):
+    singurl="https://www.t00ls.net/ajax-sign.json"
+    signdata={
+    "signsubmit":"true"
+    }
+    with open('cookiefile', 'rb') as fn:
+        session.cookies.update(pickle.load(fn))
+    formhashpage=session.post(url=singurl,data=signdata,headers=headers).text
+    if(formhashpage.find(logindata["username"])>0):
+        result="success"
+        mark=formhashpage.find("formhash=")
+        formhash=formhashpage[mark+9:mark+9+8]
+        return result,formhash
+    else:
+        return "fail"
+
+def signin(session,formhash):
     singurl="https://www.t00ls.net/ajax-sign.json"
     signdata={
     "formhash":"",
     "signsubmit":"true"
     }
-    formhashpage=session.post(url=singurl,data=signdata,headers=headers).text
-    mark=formhashpage.find("formhash=")
-    formhash=formhashpage[mark+9:mark+9+8]
+
     print(formhash)
     signdata["formhash"]=formhash
     response=session.post(url=singurl,data=signdata,headers=headers)
@@ -62,11 +77,15 @@ def webhook(result):
     webhookurl = "http://sc.ftqq.com/"
     sckey = ""#替换成自己的sckey
     requests.get(url=webhookurl + sckey + ".send?text=t00ls_signin_result&desp=" + result)
-    
+
+
 session=requests.session()
-result=login(session)
+try:
+    result,formhash=cookielogin(session)
+except:
+    result,formhash=login(session)
 if result=="success":
-    result=signin(session)
+    result=signin(session,formhash)
 else:
     print("login_error")
     result="login_error"
